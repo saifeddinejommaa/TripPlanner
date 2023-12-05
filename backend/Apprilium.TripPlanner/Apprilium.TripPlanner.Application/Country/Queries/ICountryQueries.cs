d11ef16e571999246d0e3ee1676de50e50f8c6a5
Saif Jommaa <sjommaa@globe-group.com>
2023-12-05 08:02:56 +0100

    missing class

--- backend/Apprilium.TripPlanner/Apprilium.TripPlanner.Application/Country/Queries/ICountryQueries.cs
+++ backend/Apprilium.TripPlanner/Apprilium.TripPlanner.Application/Country/Queries/ICountryQueries.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Apprilium.TripPlanner.Application.ResourceParameters;
+using Apprilium.TripPlanner.Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,9 @@ using System.Threading.Tasks;
 
 namespace Apprilium.TripPlanner.Application.Country.Queries
 {
-    internal interface ICountryQueries
+    public interface ICountryQueries
     {
+        Task<IEnumerable<Model.Country>> GetAllCountries();
+        Task<Model.Country> GetPassengerById(int countryId);
     }
 }
